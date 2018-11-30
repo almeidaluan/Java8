282bc908addecb19808ddd49b077fdfe573e69e3
luan <almeida.luan099@gmail.com>
2018-11-29 22:56:57 -0200

    Utilizando mais recursos do Stream

--- src/example/StreamCollections.java
+++ src/example/StreamCollections.java
@@ -1,8 +1,8 @@
 package example;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import model.Curso;
 
@@ -18,10 +18,46 @@ public class StreamCollections {
 		listaCursos.add(new Curso("Desenvolvedor FULLSTACK",500));
 		
 		listaCursos.sort(Comparator.comparing(Curso::getQuantidadeAlunos));
-		
-		listaCursos.stream()
-			.filter(c -> c.getQuantidadeAlunos() >= 50)
-			.forEach(listaFiltrada -> System.out.println(listaFiltrada.getNome()));
-			
+
+		// Listando cursos que possuem +=  50 alunos iterando ele e retornando os nomes
+		List<String> cursos = listaCursos.stream()
+				.filter(c -> c.getQuantidadeAlunos() >= 50)
+				.map(cursosFiltrados -> {
+					return cursosFiltrados.getNome();
+				}).collect(Collectors.toList());
+
+		System.out.println(cursos);
+
+		//Pegar os cursos que possuem a quantidade de alunos >= 100 e somar para ter o total de alunos desses cursos.
+		int soma = listaCursos.stream()
+				.filter(c -> c.getQuantidadeAlunos() >= 100)
+				.mapToInt(Curso::getQuantidadeAlunos).sum();
+
+		System.out.println(soma);
+
+		//Usando o ifpresent eu garanto que nao de exception
+		// caso nao tenha cursos > 500 alunos ele me retorna nada
+		//caso seja >= ele vai me retornar
+		Optional<Curso> curso = listaCursos.stream()
+				.filter(c -> c.getQuantidadeAlunos() >= 100)
+				.findAny();
+
+		curso.ifPresent(c -> System.out.println(c.getNome()));
+
+		//Pegando uma lista de Streams e transformando em map chave valor
+		Map<String, Integer> listaCursosMap = listaCursos.stream()
+				.filter(c -> c.getQuantidadeAlunos() >= 100)
+				.collect(Collectors.toMap(c -> c.getNome(), c -> c.getQuantidadeAlunos()));
+
+			System.out.println(listaCursosMap);
+
+			//Existe tambem o parallelStream em vez de .stream
+			// Ele mesmo vai decidir quantas threads usar e fazer
+			//todo o trabalho consumindo cpus do pc
+
+
+
+
+
 	}
 }
